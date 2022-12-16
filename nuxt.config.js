4e53105635cf27591d664857acdb7868b126b235
donaldduy-lb <59579409+donaldduy-lb@users.noreply.github.com>
2022-12-16 14:08:34 -0800

    build module

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,108 +3,100 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'qr-scanner-pwa',
+    title: "qr-scanner-pwa",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/main.css',
-  ],
+  css: ["@/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-   '@nuxt/postcss8',
-  ],
+  buildModules: ["@nuxt/postcss8", "@nuxtjs/pwa"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/pwa',
-  ],
+  modules: [],
 
   pwa: {
     meta: {
-      title: 'SA PWA',
-      author: 'Blu-Tang-Hackaclan',
+      title: "SA PWA",
+      author: "Blu-Tang-Hackaclan",
     },
     manifest: {
-      name: 'qr-scanner-pwa',
-      short_name: 'qr-scan',
-      lang: 'en',
-      display: 'standalone',
+      name: "qr-scanner-pwa",
+      short_name: "qr-scan",
+      lang: "en",
+      display: "standalone",
       icons: [
         {
-          "src": "/icons/android-chrome-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "any maskable"
+          src: "/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable",
         },
         {
-          "src": "/icons/android-chrome-384x384.png",
-          "sizes": "384x384",
-          "type": "image/png"
+          src: "/icons/android-chrome-384x384.png",
+          sizes: "384x384",
+          type: "image/png",
         },
         {
-          "src": "/icons/icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        }
+          src: "/icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
       ],
       start_url: "/",
       orientation: "portrait",
       theme_color: "#B12A34",
-      background_color: "#B12A34"
+      background_color: "#B12A34",
     },
     workbox: {
       /* workbox options */
       runtimeCaching: [
         {
-          urlPattern: 'https://fonts.googleapis.com/.*',
-          handler: 'cacheFirst',
-          method: 'GET',
-          strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+          urlPattern: "https://fonts.googleapis.com/.*",
+          handler: "cacheFirst",
+          method: "GET",
+          strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
         },
         {
-          urlPattern: 'https://fonts.gstatic.com/.*',
-          handler: 'cacheFirst',
-          method: 'GET',
-          strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+          urlPattern: "https://fonts.gstatic.com/.*",
+          handler: "cacheFirst",
+          method: "GET",
+          strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
         },
         {
-          urlPattern: 'https://cdn.snipcart.com/.*',
-          method: 'GET',
-          strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+          urlPattern: "https://cdn.snipcart.com/.*",
+          method: "GET",
+          strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
         },
         {
-          urlPattern: 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-          handler: 'cacheFirst',
-          method: 'GET',
-          strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
-        }
-      ]
-    }
+          urlPattern:
+            "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+          handler: "cacheFirst",
+          method: "GET",
+          strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
+        },
+      ],
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -115,5 +107,5 @@ export default {
         autoprefixer: {},
       },
     },
-  }
-}
+  },
+};
