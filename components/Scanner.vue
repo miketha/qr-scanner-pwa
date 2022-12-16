3dd5f544e72acc8b85e392c9d3764f42d7d55626
miketha <83609193+miketha@users.noreply.github.com>
2022-12-15 16:36:40 -0800

    Basic scanner set up

---
+++ components/Scanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+</template>
+
+<script setup>
+  import { ref } from "vue"
+  import { StreamBarcodeReader } from "vue-barcode-reader"
+
+  const decodedText = ref("")
+
+  const onLoaded = () => {
+    console.log("loaded")
+  }
+  const onDecode = (text) => {
+    decodedText.value = text
+  }
+</script>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  .information {
+    margin-top: 100px;
+  }
+</style>
